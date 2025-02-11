294d08d75ea8eaed704c797d2fa73d5a2a826b53
RatDan <herodanio@gmail.com>
2025-02-11 10:15:14 +0100

    Updated README.md

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
